6e76d3f17433544cf618c1c52403b46fa3c3c5c2
Salvador-dev <83884209+Salvador-dev@users.noreply.github.com>
2021-07-03 19:12:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 const links = document.querySelectorAll(".btn")
 const home = document.querySelector(".homebtn")
+const cart = document.querySelector(".cartbtn")
+const cart1 = document.querySelector(".cartbtn1")
+const menu = document.querySelector(".menu")
+const menubox = document.createElement("DIV")
+const cartbox = document.createElement("DIV")
+
+menubox.id = "menubox"
+document.body.appendChild(menubox)
+cartbox.id = "cartbox"
+document.body.appendChild(cartbox)
+
+const nav = document.querySelector(".ul")
+const x = document.querySelector(".x")
+
+menu.addEventListener("click", ()=>{
+   menubox.classList.add("active")
+   menubox.appendChild(x)
+   x.classList.add("active")
+   menubox.appendChild(nav)
+   nav.classList.add("active")
+})
+
+x.addEventListener("click", ()=>{
+    menubox.classList.remove('active')
+    cartbox.classList.remove('active')
+    x.classList.remove('active')
+})
+
+
+cart.addEventListener("click", ()=>{
+    cartbox.classList.add("active")
+    cartbox.appendChild(x)
+    x.classList.add("active")
+    
+ })
+
+cart1.addEventListener("click", ()=>{
+    cartbox.classList.add("active")
+    cartbox.appendChild(x)
+    x.classList.add("active")
+        
+     })
+
 
 home.addEventListener("click", ()=>{
     window.scrollTo(0,0)
@@ -9,5 +52,9 @@ links.forEach((item)=>{
     item.addEventListener("click", ()=>{
         let el = document.querySelector("." + item.getAttribute("data-link"))
         el.scrollIntoView();
+        menubox.classList.remove("active")
     })
 })
+
+
+    
