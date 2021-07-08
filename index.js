1f1f5491141e5456c637630665100dc0c097a303
Salvador-dev <83884209+Salvador-dev@users.noreply.github.com>
2021-07-07 21:28:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const links = document.querySelectorAll(".btn")
+const home = document.querySelector(".homebtn")
+const cart = document.querySelector(".cartbtn")
+const cart1 = document.querySelector(".cartbtn1")
+const menu = document.querySelector(".menu")
+const menubox = document.createElement("DIV")
+
+
+menubox.id = "menubox"
+document.body.appendChild(menubox)
+
+
+const nav = document.querySelector(".ul")
+const x = document.querySelector(".x")
+
+menu.addEventListener("click", ()=>{
+   menubox.classList.add("active")
+   menubox.appendChild(x)
+   x.classList.add("active")
+   menubox.appendChild(nav)
+   nav.classList.add("active")
+})
+
+x.addEventListener("click", ()=>{
+    menubox.classList.remove('active')
+    cartbox.classList.remove('active')
+    x.classList.remove('active')
+})
+
+
+cart.addEventListener("click", ()=>{
+    cartbox.classList.add("active")
+    cartbox.appendChild(x)
+    x.classList.add("active")
+    
+ })
+
+cart1.addEventListener("click", ()=>{
+    cartbox.classList.add("active")
+    cartbox.appendChild(x)
+    x.classList.add("active")
+        
+     })
+
+
+home.addEventListener("click", ()=>{
+    window.scrollTo(0,0)
+})
+
+links.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        let el = document.querySelector("." + item.getAttribute("data-link"))
+        el.scrollIntoView();
+        menubox.classList.remove("active")
+    })
+})
+
+const addToCart = document.querySelectorAll(".addbtn")
+
+addToCart.forEach((button) => {
+    button.addEventListener("click", addToCartClicked)
+})
+    
